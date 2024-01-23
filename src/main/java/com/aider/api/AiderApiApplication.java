cd6a9179d56dd851f2b344e5229f301c4afbf867
seongolee <seongo0521@gmail.com>
2024-01-23 15:16:43 +0900

    cache

---
+++ src/main/java/com/aider/api/AiderApiApplication.java
@@ -0,0 +1,13 @@
+package com.aider.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiderApiApplication.class, args);
+	}
+
+}
