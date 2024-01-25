7b333763ea741b89178138b68e5b2281d3e99478
park0jae <okvv26@naver.com>
2024-01-25 18:00:35 +0900

    feat: RefreshToken 생성 메소드 구현

--- core/security/src/main/java/com/aider/coresecurity/security/jwt/JwtFilter.java
+++ core/security/src/main/java/com/aider/coresecurity/security/jwt/JwtFilter.java
@@ -1,62 +1,61 @@
 package com.aider.coresecurity.security.jwt;
 
-import io.jsonwebtoken.ExpiredJwtException;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.CredentialsExpiredException;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
-import org.springframework.web.filter.OncePerRequestFilter;
+import static org.springframework.util.StringUtils.*;
+
+import java.io.IOException;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
-import static org.springframework.util.StringUtils.*;
-import static org.springframework.util.StringUtils.hasText;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.CredentialsExpiredException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
 
+import io.jsonwebtoken.ExpiredJwtException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RequiredArgsConstructor
 public class JwtFilter extends OncePerRequestFilter {
-    private final String AUTHENTICATION_HEADER = "Authorization";
-    private final String AUTHENTICATION_SCHEME = "Bearer";
+	private final String AUTHENTICATION_HEADER = "Authorization";
+	private final String AUTHENTICATION_SCHEME = "Bearer ";
 
-    private final JwtProvider jwtProvider;
+	private final JwtProvider jwtProvider;
 
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+		throws ServletException, IOException {
+		try {
+			String accessToken = extractToken(request);
+			if (hasText(accessToken) && jwtProvider.validate(accessToken)) {
+				SecurityContextHolder.getContext()
+					.setAuthentication(jwtProvider.toAuthentication(accessToken));
+			}
+			filterChain.doFilter(request, response);
+		} catch (ExpiredJwtException e) {
+			log.warn(">>> Jwt Token Expired !!!");
+			throw new CredentialsExpiredException("토큰의 유효기간이 만료되었습니다.", e);
+		} catch (Exception e) {
+			log.warn(">>> Failed Token Authenticate !!!");
+			throw new BadCredentialsException("토큰 인증에 실패하였습니다.");
+		}
+	}
 
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-            throws ServletException, IOException {
-        try{
-            String accessToken = extractToken(request);
-            if(hasText(accessToken) && jwtProvider.validate(accessToken)) {
-                SecurityContextHolder.getContext()
-                        .setAuthentication(jwtProvider.toAuthentication(accessToken));
-            }
-            filterChain.doFilter(request, response);
-        }catch(ExpiredJwtException e) {
-            throw new CredentialsExpiredException("토큰의 유효기간이 만료되었습니다.", e);
-        }catch(Exception e) {
-            throw new BadCredentialsException("토큰 인증에 실패하였습니다.");
-        }
-    }
+	/**
+	 * 토큰 추출
+	 */
+	private String extractToken(HttpServletRequest request) {
+		String bearerToken = request.getHeader(AUTHENTICATION_HEADER);
 
-    /**
-     * 토큰 추출
-     */
-    private String extractToken(HttpServletRequest request) {
-        String bearerToken = request.getHeader(AUTHENTICATION_HEADER);
+		if (hasText(bearerToken) && bearerToken.startsWith(AUTHENTICATION_SCHEME)) {
+			return bearerToken.substring(AUTHENTICATION_SCHEME.length());
+		}
 
-        if(hasText(bearerToken) && bearerToken.startsWith(AUTHENTICATION_SCHEME + " ")){
-            return bearerToken.substring(AUTHENTICATION_SCHEME.length() + 1);
-        }
-
-        throw new AuthenticationCredentialsNotFoundException("토큰이 존재하지 않습니다.");
-    }
+		throw new AuthenticationCredentialsNotFoundException("토큰이 존재하지 않습니다.");
+	}
 }
--- core/security/src/main/java/com/aider/coresecurity/security/jwt/JwtProvider.java
+++ core/security/src/main/java/com/aider/coresecurity/security/jwt/JwtProvider.java
@@ -1,11 +1,13 @@
 package com.aider.coresecurity.security.jwt;
 
-import com.aider.coresecurity.security.service.UserDetailsImpl;
-import io.jsonwebtoken.*;
-import io.jsonwebtoken.io.Decoders;
-import io.jsonwebtoken.security.Keys;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.crypto.SecretKey;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -14,100 +16,121 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
-import javax.crypto.SecretKey;
-import java.time.Instant;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
-import java.util.stream.Collectors;
+import com.aider.coresecurity.security.service.UserDetailsImpl;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class JwtProvider {
-    private final String AUTHENTICATION_CLAIM_NAME = "roles";
+	private final String AUTHENTICATION_CLAIM_NAME = "roles";
 
-    @Value("${jwt.secret-key}")
-    private String accessSecret;
+	@Value("${jwt.secret-key}")
+	private String accessSecret;
 
-    @Value("${jwt.expiry-seconds}")
-    private int expirySeconds;
+	@Value("${jwt.expiry-seconds}")
+	private int expirySeconds;
 
-    /**
-     * 토큰 생성
-     */
-    public String createToken(UserDetailsImpl userDetails) {
-        Instant now = Instant.now();
-        Date expiration = Date.from(now.plusSeconds(expirySeconds));
-        SecretKey key = extractSecretKey();
+	@Value("${jwt.refresh-expiry-seconds}")
+	private int refreshExpirySeconds;
 
-        StringBuilder roles = new StringBuilder();
-        if(userDetails.getAuthorities() != null && !userDetails.getAuthorities().isEmpty()) {
-            roles.append(
-                    userDetails.getAuthorities().stream()
-                            .map(GrantedAuthority::getAuthority)
-                            .collect(Collectors.joining(", "))
-            );
-        }
+	/**
+	 * Access 토큰 생성
+	 */
+	public String createAccessToken(UserDetailsImpl userDetails) {
+		Instant now = Instant.now();
+		Date expiration = Date.from(now.plusSeconds(expirySeconds));
+		SecretKey key = extractSecretKey();
 
-        return Jwts.builder()
-                .claim("id", userDetails.getId())
-                .setSubject(userDetails.getUsername())
-                .setIssuedAt(Date.from(now))
-                .setExpiration(expiration)
-                .claim(AUTHENTICATION_CLAIM_NAME, roles.toString())
-                .signWith(key, SignatureAlgorithm.HS512)
-                .compact();
-    }
+		StringBuilder roles = new StringBuilder();
+		if (userDetails.getAuthorities() != null && !userDetails.getAuthorities().isEmpty()) {
+			roles.append(
+				userDetails.getAuthorities().stream()
+					.map(GrantedAuthority::getAuthority)
+					.collect(Collectors.joining(", "))
+			);
+		}
 
-    /**
-     * 권한 체크
-     */
-    public Authentication toAuthentication(String token) {
-        JwtParser jwtParser = Jwts.parserBuilder()
-                .setSigningKey(extractSecretKey())
-                .build();
-        Claims claims = jwtParser.parseClaimsJws(token).getBody();
+		return Jwts.builder()
+			.claim("id", userDetails.getId())
+			.setSubject(userDetails.getUsername())
+			.setIssuedAt(Date.from(now))
+			.setExpiration(expiration)
+			.claim(AUTHENTICATION_CLAIM_NAME, roles.toString())
+			.signWith(key, SignatureAlgorithm.HS512)
+			.compact();
+	}
 
-        Object roles = claims.get(AUTHENTICATION_CLAIM_NAME);
-        List<SimpleGrantedAuthority> authorities = null;
-        if(roles != null && roles.toString().trim().isEmpty()) {
-            authorities = Arrays.stream(roles.toString().split(","))
-                    .map(String::trim)
-                    .map(SimpleGrantedAuthority::new)
-                    .collect(Collectors.toList());
-        }
+	/**
+	 * Refresh 토큰 생성
+	 */
+	public String createRefreshToken() {
+		Instant expirationTime = Instant.now().plusSeconds(refreshExpirySeconds);
 
-        UserDetails user = UserDetailsImpl.builder()
-                .id(claims.get("id", Long.class))
-                .email(claims.getSubject())
-                .password(null)
-                .authorities(authorities)
-                .build();
+		return Jwts.builder()
+			.setIssuedAt(Date.from(Instant.now()))
+			.setExpiration(Date.from(expirationTime))
+			.signWith(extractSecretKey())
+			.compact();
+	}
 
-        return new UsernamePasswordAuthenticationToken(user, token, authorities);
-    }
+	/**
+	 * 권한 체크
+	 */
+	public Authentication toAuthentication(String token) {
+		JwtParser jwtParser = Jwts.parserBuilder()
+			.setSigningKey(extractSecretKey())
+			.build();
+		Claims claims = jwtParser.parseClaimsJws(token).getBody();
 
-    /**
-     * 토큰 검증
-     */
-    public Boolean validate(String token) {
-        try{
-            JwtParser jwtParser = Jwts.parserBuilder()
-                    .setSigningKey(extractSecretKey())
-                    .build();
-            jwtParser.parse(token);
+		Object roles = claims.get(AUTHENTICATION_CLAIM_NAME);
+		List<SimpleGrantedAuthority> authorities = null;
+		if (roles != null && roles.toString().trim().isEmpty()) {
+			authorities = Arrays.stream(roles.toString().split(","))
+				.map(String::trim)
+				.map(SimpleGrantedAuthority::new)
+				.collect(Collectors.toList());
+		}
 
-            return true;
-        }catch (JwtException e) {
-            return false;
-        }
-    }
+		UserDetails user = UserDetailsImpl.builder()
+			.id(claims.get("id", Long.class))
+			.email(claims.getSubject())
+			.password(null)
+			.authorities(authorities)
+			.build();
 
-    /**
-     * SecretKey 추출
-     */
-    private SecretKey extractSecretKey() {
-        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(accessSecret));
-    }
+		return new UsernamePasswordAuthenticationToken(user, token, authorities);
+	}
+
+	/**
+	 * 토큰 검증
+	 */
+	public Boolean validate(String token) {
+		try {
+			JwtParser jwtParser = Jwts.parserBuilder()
+				.setSigningKey(extractSecretKey())
+				.build();
+			jwtParser.parse(token);
+
+			return true;
+		} catch (JwtException e) {
+			return false;
+		}
+	}
+
+	/**
+	 * SecretKey 추출
+	 */
+	private SecretKey extractSecretKey() {
+		return Keys.hmacShaKeyFor(Decoders.BASE64.decode(accessSecret));
+	}
 }
