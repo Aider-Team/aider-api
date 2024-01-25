44f2ddba60285940ed4477c13cc81a69d8c25682
seongolee <seongo0521@gmail.com>
2024-01-25 14:30:53 +0900

    기본 시큐리티 사용하지 않음

--- api/member/src/main/java/com/aider/member/MemberApplication.java
+++ api/member/src/main/java/com/aider/member/MemberApplication.java
@@ -1,13 +1,14 @@
-package com.aider.memberApi;
+package com.aider.member;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @ComponentScan(basePackages = {"com.aider"})
-public class MemberApiApplication {
+public class MemberApplication {
     public static void main(String[] args) {
-        SpringApplication.run(MemberApiApplication.class, args);
+        SpringApplication.run(MemberApplication.class, args);
     }
 }
